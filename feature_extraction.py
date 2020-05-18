27f76dad37cd4a28ebee56fce5882e263654cf0c
David Li <davidli2881@gmail.com>
2020-05-18 00:05:27 -0400

    mfccs implemented, not validated yet

--- feature_extraction.py
+++ feature_extraction.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import math
 from read_wave import *
 from file_manage import *
+from scipy.fftpack import dct
 
 class FeatureObj:
     def __init__(self, signal, sampleRate, filename):
@@ -19,7 +20,7 @@ class FeatureObj:
         self.zeroCrossingRate = self.calcZeroCrossingrate() # float
         self.fft = self.calcFFT(self.signal)                # (response_y, frequency_x)
         self.filterBanks = self.calcFilterBanks()           #
-        self.mfcc = self.calcMFCC()                         #
+        self.mfcc = self.calcMFCC()         #
 
     def calcZeroCrossingrate(self):
         # Formula from : https://en.wikipedia.org/wiki/Zero-crossing_rate
@@ -105,7 +106,7 @@ class FeatureObj:
 
         ''' GENERATE TRIANGULAR FILTERS '''
         lowFreq = 300
-        hiFreq = 10500
+        hiFreq = 10000
         numFilters = 26
 
         lowMel = self.mel(lowFreq)
@@ -123,7 +124,7 @@ class FeatureObj:
         for frame in fft_frames:
             filter_values = []
             for filter in filters:
-                filter_values.append(math.log(self.applyFilter(filter, frame), 10))
+                filter_values.append(20 * math.log(self.applyFilter(filter, frame), 10))
 
             filterBanks.append(filter_values)
 
@@ -148,91 +149,9 @@ class FeatureObj:
                 g. Take the log of each of the 26 coefficients to get filterbank energies
                 h. Take DCT of each energy to get MFCC (only keep the lower 13)
         '''
-        
-        return
+        mfcc = np.transpose(self.filterBanks.copy())
 
+        for i in range(len(mfcc)):
+            mfcc[i] = dct(mfcc[i], type=2, norm='ortho')
 
-def plotFeatures(featureObjs):
-    numCols = len(featureObjs) # show these along the horizontal (columns)
-    numRows = 4 # Time domain, FFT, frequency banks, MFCC
-
-    fig, ax = plt.subplots(nrows=numRows, ncols=numCols, sharey=False, figsize=(16, 9))
-
-    plt.subplots_adjust(left=0.075, right=.975, top=0.9, bottom=0.05)
-    fig.suptitle('Feature Plots')
-    for i in range(numCols):
-        ax[0, i].set_title(featureObjs[i].filename)
-
-    ax[0, 0].set_ylabel('Time Domain')
-    ax[1, 0].set_ylabel('FFT')
-    ax[2, 0].set_ylabel('Filter Banks')
-    ax[3, 0].set_ylabel('MFCC')
-
-    for i in range(numCols):
-        # Plot time domain signal
-        frames = range(len(featureObjs[i].signal))
-        time = [frame / featureObjs[i].sampleRate for frame in frames]
-        ax[0, i].plot(time, featureObjs[i].signal, '#ff7878')
-        ax[0, i].set_facecolor('#e0e0e0')
-
-    for i in range(numCols):
-        # Plot FFTs of entire signal
-        ax[1, i].plot(featureObjs[i].fft[1], featureObjs[i].fft[0], '#ff7878')
-        ax[1, i].set_facecolor('#e0e0e0')
-
-    for i in range(numCols):
-        # Plot Filter Banks
-        ax[2, i].imshow(featureObjs[i].filterBanks, cmap='winter', interpolation='nearest', aspect='auto')
-
-def plotFeature(featureObj):
-    numRows = 4 # Time domain, FFT, frequency banks, MFCC
-
-    fig, ax = plt.subplots(nrows=numRows, ncols=1, sharey=False, figsize=(16, 9))
-
-    plt.subplots_adjust(left=0.075, right=.975, top=0.9, bottom=0.05)
-    fig.suptitle('Feature Plots')
-
-    ax[0].set_title(featureObj.filename)
-
-    ax[0].set_ylabel('Time Domain')
-    ax[1].set_ylabel('FFT')
-    ax[2].set_ylabel('Filter Banks')
-    ax[3].set_ylabel('MFCC')
-
-    # Plot time domain signal
-    frames = range(len(featureObj.signal))
-    time = [frame / featureObj.sampleRate for frame in frames]
-    ax[0].plot(time, featureObj.signal, '#ff7878')
-    ax[0].set_facecolor('#e0e0e0')
-
-
-    # Plot FFTs of entire signal
-    ax[1].plot(featureObj.fft[1], featureObj.fft[0], '#ff7878')
-    ax[1].set_facecolor('#e0e0e0')
-
-
-    # Plot Filter Banks
-    ax[2].imshow(featureObj.filterBanks, cmap='winter', interpolation='nearest', extend=[0, 100, 0, 1], aspect='auto')
-
-# data1, sr1 = read_wave(test_file_1)
-# data2, sr2 = read_wave(test_file_2)
-# data3, sr3 = read_wave(test_file_3)
-# data4, sr4 = read_wave(test_file_4)
-# data5, sr5 = read_wave(test_file_5)
-#
-# data1 = trimData(data1, 0.001)
-# data2 = trimData(data2, 0.001)
-# data3 = trimData(data3, 0.001)
-# data4 = trimData(data4, 0.001)
-# data5 = trimData(data5, 0.001)
-#
-# f1 = FeatureObj(data1, sr1, test_file_1)
-# print("Sample 1 done")
-# f2 = FeatureObj(data2, sr2, test_file_2)
-# print("Sample 2 done")
-# f3 = FeatureObj(data3, sr3, test_file_3)
-# print("Sample 3 done")
-# f4 = FeatureObj(data4, sr4, test_file_4)
-# print("Sample 4 done")
-# f5 = FeatureObj(data5, sr5, test_file_5)
-# print("Sample 5 done")
+        return np.transpose(mfcc)[:13]
--- plot_manage.py
+++ plot_manage.py
@@ -1,6 +1,69 @@
 from feature_extraction import *
 from file_manage import *
 
+def plotFeatures(featureObjs):
+    numCols = len(featureObjs) # show these along the horizontal (columns)
+    numRows = 4 # Time domain, FFT, frequency banks, MFCC
+
+    fig, ax = plt.subplots(nrows=numRows, ncols=numCols, sharey=False, figsize=(16, 9))
+
+    plt.subplots_adjust(left=0.075, right=.975, top=0.9, bottom=0.05)
+    fig.suptitle('Feature Plots')
+    for i in range(numCols):
+        ax[0, i].set_title(featureObjs[i].filename)
+
+    ax[0, 0].set_ylabel('Time Domain')
+    ax[1, 0].set_ylabel('FFT')
+    ax[2, 0].set_ylabel('Filter Banks')
+    ax[3, 0].set_ylabel('MFCC')
+
+    for i in range(numCols):
+        # Plot time domain signal
+        frames = range(len(featureObjs[i].signal))
+        time = [frame / featureObjs[i].sampleRate for frame in frames]
+        ax[0, i].plot(time, featureObjs[i].signal, '#ff7878')
+        ax[0, i].set_facecolor('#e0e0e0')
+
+    for i in range(numCols):
+        # Plot FFTs of entire signal
+        ax[1, i].plot(featureObjs[i].fft[1], featureObjs[i].fft[0], '#ff7878')
+        ax[1, i].set_facecolor('#e0e0e0')
+
+    for i in range(numCols):
+        # Plot Filter Banks
+        pos = ax[2, i].imshow(featureObjs[i].filterBanks, cmap='winter', interpolation='nearest', aspect='auto')
+        fig.colorbar(pos, ax=ax[2, i])
+
+    for i in range(numCols):
+        # Plot MFCCs
+        pos = ax[3, i].imshow(featureObjs[i].mfcc, cmap='winter', interpolation='nearest', aspect='auto')
+        fig.colorbar(pos, ax=ax[3, i])
+
+def plot_test_files():
+    data1, sr1 = read_wave(test_file_1)
+    data2, sr2 = read_wave(test_file_2)
+    data3, sr3 = read_wave(test_file_3)
+    data4, sr4 = read_wave(test_file_4)
+    data5, sr5 = read_wave(test_file_5)
+
+    data1 = trimData(data1, 0.001)
+    data2 = trimData(data2, 0.001)
+    data3 = trimData(data3, 0.001)
+    data4 = trimData(data4, 0.001)
+    data5 = trimData(data5, 0.001)
+
+    f1 = FeatureObj(data1, sr1, test_file_1)
+    print("Sample 1 done")
+    f2 = FeatureObj(data2, sr2, test_file_2)
+    print("Sample 2 done")
+    f3 = FeatureObj(data3, sr3, test_file_3)
+    print("Sample 3 done")
+    f4 = FeatureObj(data4, sr4, test_file_4)
+    print("Sample 4 done")
+    f5 = FeatureObj(data5, sr5, test_file_5)
+    print("Sample 5 done")
+
+    plotFeatures([f1, f2, f3, f4, f5])
 
 def plot_claps():
     clapData1, sr1 = read_wave(clap_file_1)
@@ -16,18 +79,51 @@ def plot_claps():
     clapData5 = trimData(clapData5, 0.0005)
 
     f1 = FeatureObj(clapData1, sr1, clap_file_1)
-    print("Sample 1 done")
     f2 = FeatureObj(clapData2, sr2, clap_file_2)
-    print("Sample 2 done")
     f3 = FeatureObj(clapData3, sr3, clap_file_3)
-    print("Sample 3 done")
     f4 = FeatureObj(clapData4, sr4, clap_file_4)
-    print("Sample 4 done")
     f5 = FeatureObj(clapData5, sr5, clap_file_5)
-    print("Sample 5 done")
 
     plotFeatures([f1, f2, f3, f4, f5])
 
-    plt.show()
+def plot_crashes():
+    crashData1, sr1 = read_wave(crash_file_1)
+    crashData2, sr2 = read_wave(crash_file_2)
+    crashData3, sr3 = read_wave(crash_file_3)
+    crashData4, sr4 = read_wave(crash_file_4)
+    crashData5, sr5 = read_wave(crash_file_5)
 
-plot_claps()
+    crashData1 = trimData(crashData1, 0.0005)
+    crashData2 = trimData(crashData2, 0.0005)
+    crashData3 = trimData(crashData3, 0.0005)
+    crashData4 = trimData(crashData4, 0.0005)
+    crashData5 = trimData(crashData5, 0.0005)
+
+    f1 = FeatureObj(crashData1, sr1, crash_file_1)
+    f2 = FeatureObj(crashData2, sr2, crash_file_2)
+    f3 = FeatureObj(crashData3, sr3, crash_file_3)
+    f4 = FeatureObj(crashData4, sr4, crash_file_4)
+    f5 = FeatureObj(crashData5, sr5, crash_file_5)
+
+    plotFeatures([f1, f2, f3, f4, f5])
+
+def plot_hihats():
+    hihatData1, sr1 = read_wave(hihat_file_1)
+    hihatData2, sr2 = read_wave(hihat_file_2)
+    hihatData3, sr3 = read_wave(hihat_file_3)
+    hihatData4, sr4 = read_wave(hihat_file_4)
+    hihatData5, sr5 = read_wave(hihat_file_5)
+
+    hihatData1 = trimData(hihatData1, 0.0005)
+    hihatData2 = trimData(hihatData2, 0.0005)
+    hihatData3 = trimData(hihatData3, 0.0005)
+    hihatData4 = trimData(hihatData4, 0.0005)
+    hihatData5 = trimData(hihatData5, 0.0005)
+
+    f1 = FeatureObj(hihatData1, sr1, hihat_file_1)
+    f2 = FeatureObj(hihatData2, sr2, hihat_file_2)
+    f3 = FeatureObj(hihatData3, sr3, hihat_file_3)
+    f4 = FeatureObj(hihatData4, sr4, hihat_file_4)
+    f5 = FeatureObj(hihatData5, sr5, hihat_file_5)
+
+    plotFeatures([f1, f2, f3, f4, f5])
